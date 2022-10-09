c299efaaceb169fd81539380440f3f603c974e21
Konrad <konslo96@gmail.com>
2022-10-09 21:01:05 +0200

    [feat] Add Navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { Route, Router, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<Navigate replace to="/regular" />} />
         <Route path="regular" />
